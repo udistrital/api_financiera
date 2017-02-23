42806e218f6a21d1a509f3fb9ad4eddcadfccf09
jotavargas <jotavargas89@gmail.com>
2017-02-23 10:08:41 -0500

    ajustes a script de instalación general y creacion de script de intalación de solo datos

--- db/installData.sql
+++ db/installData.sql
@@ -1,10 +1,3 @@
-\echo '\n#################### Creacion BD Financiera #################### \n'
-
-\i financiera_db/union_financiera_22_02_2017.sql
-
-\echo '\n#################### Fin Creacion BD Financiera #################### \n'
-
-
 \echo '\n#################### Datos Financiera #################### \n'
 
 \i data/data_estado_orden_pago.sql
