b27741c1bb0e84ae082bd3208a0cc9b862b20489
faparraf <faparraf@gmail.com>
2017-03-27 16:14:44 -0500

    activación de swagger

--- main.go
+++ main.go
@@ -14,10 +14,10 @@ func init() {
 
 func main() {
 	orm.Debug = true
-	/*if beego.BConfig.RunMode == "dev" {
+	if beego.BConfig.RunMode == "dev" {
 		beego.BConfig.WebConfig.DirectoryIndex = true
 		beego.BConfig.WebConfig.StaticDir["/swagger"] = "swagger"
-	}*/
+	}
 
 	beego.InsertFilter("*", beego.BeforeRouter, cors.Allow(&cors.Options{
 		AllowOrigins: []string{"*"},
