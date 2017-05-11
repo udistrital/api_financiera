04071cb612e44f36db5f3e48ed9a1fce37a487fe
jotavargas <jotavargas89@gmial.com>
2017-05-11 10:27:17 -0500

    Referencia a tabla registro_presupuestal_disponibilidad_apropiacion como objeto en el json del servicio

--- models/concepto_orden_pago.go
+++ models/concepto_orden_pago.go
@@ -10,11 +10,11 @@ import (
 )
 
 type ConceptoOrdenPago struct {
-	Id                                               int        `orm:"column(id);pk;auto"`
-	Valor                                            float64    `orm:"column(valor)"`
-	Concepto                                         *Concepto  `orm:"column(concepto);rel(fk)"`
-	OrdenDePago                                      *OrdenPago `orm:"column(orden_de_pago);rel(fk)"`
-	RegistroPresupuestalDisponibilidadApropiacion    int        `orm:"column(registro_presupuestal_disponibilidad_apropiacion)"`
+	Id                                               int                                                   `orm:"column(id);pk;auto"`
+	Valor                                            float64                                               `orm:"column(valor)"`
+	Concepto                                         *Concepto                                             `orm:"column(concepto);rel(fk)"`
+	OrdenDePago                                      *OrdenPago                                            `orm:"column(orden_de_pago);rel(fk)"`
+	RegistroPresupuestalDisponibilidadApropiacion    *RegistroPresupuestalDisponibilidadApropiacion        `orm:"column(registro_presupuestal_disponibilidad_apropiacion);rel(fk);null"`
 }
 
 func (t *ConceptoOrdenPago) TableName() string {
