0b33950b29246b1ad3ec30d00297ca069b36c64a
miguelramirez93 <darckzios@gmail.com>
2017-10-06 15:30:46 -0500

    #16731# se aplica la funcion de optimizacion por digest en el armado del arbol de rubros del crud api para probar la flexibilidad de la funcion generada.

--- utilidades/jsontools.go
+++ utilidades/jsontools.go
@@ -1,9 +1,54 @@
 package utilidades
 
-import "encoding/json"
+import (
+	"encoding/json"
+	"sync"
+)
 
 func FillStruct(m interface{}, s interface{}) (err error) {
 	j, _ := json.Marshal(m)
 	err = json.Unmarshal(j, s)
 	return
+}
+
+//funcion para generar canales de interface{}
+func GenChanInterface(mp ...interface{}) <-chan interface{} {
+	out := make(chan interface{})
+	go func() {
+		for _, ch := range mp {
+			out <- ch
+		}
+		close(out)
+	}()
+	return out
+}
+
+func digester(done <-chan interface{}, f func(interface{}) interface{}, in <-chan interface{}, out chan<- interface{}) {
+	for intfc := range in {
+		res := f(intfc)
+		select {
+		case out <- res:
+		case <-done:
+			return
+		}
+	}
+}
+
+//funcion para administrar las go rutines armadas para la consulta de solicitudes de rp.
+func Digest(done <-chan interface{}, f func(interface{}) interface{}, in <-chan interface{}) (outchan <-chan interface{}) {
+	out := make(chan interface{})
+	var wg sync.WaitGroup
+	const numDigesters = 20
+	wg.Add(numDigesters)
+	for i := 0; i < numDigesters; i++ {
+		go func() {
+			digester(done, f, in, out)
+			wg.Done()
+		}()
+	}
+	go func() {
+		wg.Wait()
+		close(out)
+	}()
+	return out
 }
