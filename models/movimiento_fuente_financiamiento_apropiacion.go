3bcae947aa4da693e7d9d9e9d15f6048489f88c9
JohanFornari <johandebonaire@hotmail.com>
2017-10-14 14:44:01 -0500

    Update movimiento_fuente_financiamiento_apropiacion.go

--- models/movimiento_fuente_financiamiento_apropiacion.go
+++ models/movimiento_fuente_financiamiento_apropiacion.go
@@ -18,7 +18,7 @@ type MovimientoFuenteFinanciamientoApropiacion struct {
 	Descripcion                               string                           `orm:"column(descripcion);null"`
 	FuenteFinanciamientoApropiacion           *FuenteFinanciamientoApropiacion `orm:"column(fuente_financiamiento_apropiacion);rel(fk)"`
 	MovimientoFuenteFinanciamientoApropiacion int                              `orm:"column(movimiento_fuente_financiamiento_apropiacion);null"`
-	TipoDocumento                             int          										 `orm:"column(tipo_documento)"`
+	TipoDocumento                             int         			   `orm:"column(tipo_documento)"`
 }
 
 func (t *MovimientoFuenteFinanciamientoApropiacion) TableName() string {
