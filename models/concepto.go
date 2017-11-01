5c305a5c932e1c09cbbf70fd4ccf92e4c5a6c53d
miguelramirez93 <darckzios@gmail.com>
2017-11-01 12:22:18 -0500

    se agrega loadrelated homolagacion concepto

--- models/concepto.go
+++ models/concepto.go
@@ -55,7 +55,7 @@ func GetConceptoById(id int) (v *Concepto, err error) {
 func GetAllConcepto(query map[string]string, fields []string, sortby []string, order []string,
 	offset int64, limit int64) (ml []interface{}, err error) {
 	o := orm.NewOrm()
-	qs := o.QueryTable(new(Concepto))
+	qs := o.QueryTable(new(Concepto)).RelatedSel(5)
 	// query k=v
 	for k, v := range query {
 		// rewrite dot-notation to Object__Attribute
