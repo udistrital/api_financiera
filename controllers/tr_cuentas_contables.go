3353b08187ce5a0b935d2ea725b5c083137e8e72
geiner <geiner.salcedo.ppc0111@gmail.com>
2017-11-21 16:29:07 -0500

    se construye servicio para edicion de cuentas transaccionalmente, con la opcion de cmbiar de padre y verificar si la cuenta ha sido usada

--- controllers/tr_cuentas_contables.go
+++ controllers/tr_cuentas_contables.go
@@ -3,6 +3,7 @@ package controllers
 import (
 	"encoding/json"
 	"fmt"
+	"strconv"
 
 	"github.com/fatih/structs"
 	"github.com/udistrital/api_financiera/models"
@@ -44,6 +45,39 @@ func (c *TrCuentasContablesController) Post() {
 		}
 	} else {
 		fmt.Println(err)
+		c.Data["json"] = models.Alert{Type: "error", Code: "E_0458", Body: err.Error()}
+	}
+	c.ServeJSON()
+}
+
+// Put ...
+// @Title Put
+// @Description update the TrCuentaContable
+// @Param	id		path 	string	true		"The id you want to update"
+// @Param	body		body 	models.TrCuentaContable	true		"body for TrCuentaContable content"
+// @Success 200 {object} models.TrCuentaContable
+// @Failure 403 :id is not int
+// @router /:id [put]
+func (c *TrCuentasContablesController) Put() {
+	idStr := c.Ctx.Input.Param(":id")
+	id, _ := strconv.Atoi(idStr)
+	fmt.Print(id)
+	var v models.TrCuentaContable
+	if err := json.Unmarshal(c.Ctx.Input.RequestBody, &v); err == nil {
+		if err, cod := models.UpdateTrCuentaContable(&v); err == nil {
+			c.Ctx.Output.SetStatus(201)
+			c.Data["json"] = models.Alert{Type: "success", Code: "S_542", Body: cod}
+		} else {
+			alertdb := structs.Map(err)
+			var code string
+			utilidades.FillStruct(alertdb["Code"], &code)
+			alert := models.Alert{Type: "error", Code: "E_" + code, Body: err.Error()}
+			if err.Error() == "04566" {
+				alert = models.Alert{Type: "error", Code: "E_" + err.Error(), Body: nil}
+			}
+			c.Data["json"] = alert
+		}
+	} else {
 		c.Data["json"] = models.Alert{Type: "error", Code: "E_0458", Body: err.Error()}
 	}
 	c.ServeJSON()
--- models/tr_cuentas_contables.go
+++ models/tr_cuentas_contables.go
@@ -1,6 +1,11 @@
 package models
 
-import "github.com/astaxie/beego/orm"
+import (
+	"errors"
+	"fmt"
+
+	"github.com/astaxie/beego/orm"
+)
 
 type TrCuentaContable struct {
 	Cuenta      *CuentaContable
@@ -38,4 +43,53 @@ func AddTransaccionCuentaContable(m *TrCuentaContable) (err error) {
 		o.Rollback()
 	}
 	return
+}
+
+// UpdateTrCuentaContable updates CuentaContable by Id and returns error,cod  if the record to be updated doesn't exist
+func UpdateTrCuentaContable(m *TrCuentaContable) (err error, cod string) {
+	o := orm.NewOrm()
+	o.Begin()
+
+	v := CuentaContable{Id: m.Cuenta.Id}
+	// ascertain id exists in the database
+	if err = o.Read(&v); err == nil {
+		var back int64
+		if o.Raw(`select id from financiera.movimiento_contable where cuenta_contable = ?
+							union
+							select id from financiera.concepto_tesoral_cuenta_contable where cuenta_contable= ?`, v.Id, v.Id).QueryRow(&back); back > 0 {
+			err = errors.New("04566")
+			o.Rollback()
+			return err, ""
+		} else {
+			if m.CuentaPadre != nil {
+				var ec EstructuraCuentas
+				if err = o.Raw("select * from financiera.estructura_cuentas where cuenta_hijo=? and plan_cuentas=?", m.Cuenta.Id, m.PlanCuentas.Id).QueryRow(&ec); err == nil {
+					ec.CuentaPadre = m.CuentaPadre
+					if _, err = o.Update(&ec); err == nil {
+						m.Cuenta.Codigo = m.CuentaPadre.Codigo + "-" + m.Cuenta.Codigo[len(m.Cuenta.Codigo)-int(m.Cuenta.NivelClasificacion.Longitud):]
+						fmt.Print(m.Cuenta.Codigo)
+					} else {
+						o.Rollback()
+						return err, ""
+					}
+				} else {
+					o.Rollback()
+					return err, ""
+				}
+			} else {
+				m.Cuenta.Codigo = v.Codigo[:len(v.Codigo)-int(m.Cuenta.NivelClasificacion.Longitud)] + m.Cuenta.Codigo[len(m.Cuenta.Codigo)-int(m.Cuenta.NivelClasificacion.Longitud):]
+				fmt.Print("codigo-->", m.Cuenta.Codigo)
+			}
+			if _, err = o.Update(m.Cuenta); err == nil {
+				o.Commit()
+				return nil, m.Cuenta.Codigo
+			} else {
+				o.Rollback()
+				return err, ""
+			}
+		}
+	} else {
+		o.Rollback()
+		return err, ""
+	}
 }
