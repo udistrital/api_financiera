365ed607609df61c7111f5828c8063f90138712a
jjvargass <jotavargas89@gmail.com>
2018-01-11 09:36:57 -0500

    #18991# asociar ss en homologacion API

--- models/homologacion_concepto.go
+++ models/homologacion_concepto.go
@@ -21,12 +21,13 @@ type RpCdpRubroConceptoValor struct {
 }
 
 type HomologacionConcepto struct {
-	Id             int       `orm:"column(id);pk;auto"`
-	Vigencia       float64   `orm:"column(vigencia)"`
-	FechaCreacion  time.Time `orm:"column(fecha_creacion);type(date)"`
-	ConceptoKronos *Concepto `orm:"column(concepto_kronos);rel(fk)"`
-	ConceptoTitan  int       `orm:"column(concepto_titan)"`
-	NominaTitan    int       `orm:"column(nomina_titan)"`
+	Id              int       `orm:"column(id);pk;auto"`
+	Vigencia        float64   `orm:"column(vigencia)"`
+	FechaCreacion   time.Time `orm:"column(fecha_creacion);type(date)"`
+	ConceptoKronos  *Concepto `orm:"column(concepto_kronos);rel(fk)"`
+	ConceptoTitan   int       `orm:"column(concepto_titan)"`
+	NominaTitan     int       `orm:"column(nomina_titan)"`
+	SeguridadSocial bool      `orm:"column(seguridad_social)"`
 }
 
 func (t *HomologacionConcepto) TableName() string {
