90e239c2ad496dd4f6c0a5a5fbcf2b77120c6b2e
Fernando Torres <fernandotower@gmail.com>
2018-02-26 12:39:41 -0500

    Actualizando punto de entrada

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,8 +5,8 @@ set -u
 set -o pipefail
 
 if [ -n "${PARAMETER_STORE:-}" ]; then
-  export FINANCIERA_API_CRUD__PGUSER="$(aws ssm get-parameter --name /${PARAMETER_STORE}/api_financiera/db/username --output text --query Parameter.Value)"
-  export FINANCIERA_API_CRUD__PGPASS="$(aws ssm get-parameter --with-decryption --name /${PARAMETER_STORE}/api_financiera/db/password --output text --query Parameter.Value)"
+  export API_FINANCIERA_CRUD__PGUSER="$(aws ssm get-parameter --name /${PARAMETER_STORE}/api_financiera/db/username --output text --query Parameter.Value)"
+  export API_FINANCIERA_CRUD__PGPASS="$(aws ssm get-parameter --with-decryption --name /${PARAMETER_STORE}/api_financiera/db/password --output text --query Parameter.Value)"
 fi
 
 exec ./main "$@"
