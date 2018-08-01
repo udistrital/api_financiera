cc50aefaef120a263cadbee6e7e0c4059d917a3c
Yeison <sistemas@localhost.localdomain>
2018-08-01 09:27:28 -0500

    ajustes obtener OP por RP

--- models/orden_pago_registro_presupuestal.go
+++ models/orden_pago_registro_presupuestal.go
@@ -49,7 +49,7 @@ func GetOrdenPagoRegistroPresupuestalById(id int) (v *OrdenPagoRegistroPresupues
 func GetAllOrdenPagoRegistroPresupuestal(query map[string]string, fields []string, sortby []string, order []string,
 	offset int64, limit int64) (ml []interface{}, err error) {
 	o := orm.NewOrm()
-	qs := o.QueryTable(new(OrdenPagoRegistroPresupuestal))
+	qs := o.QueryTable(new(OrdenPagoRegistroPresupuestal)).RelatedSel(5)
 	// query k=v
 	for k, v := range query {
 		// rewrite dot-notation to Object__Attribute
