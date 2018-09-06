f9c4e49115be591dedbe806ff6dc5e3d7d46388a
Yeison <sistemas@localhost.localdomain>
2018-09-06 16:50:02 -0500

    obtener informacion cuenta y op

--- models/orden_pago_cuenta_especial.go
+++ models/orden_pago_cuenta_especial.go
@@ -52,7 +52,7 @@ func GetOrdenPagoCuentaEspecialById(id int) (v *OrdenPagoCuentaEspecial, err err
 func GetAllOrdenPagoCuentaEspecial(query map[string]string, fields []string, sortby []string, order []string,
 	offset int64, limit int64) (ml []interface{}, err error) {
 	o := orm.NewOrm()
-	qs := o.QueryTable(new(OrdenPagoCuentaEspecial))
+	qs := o.QueryTable(new(OrdenPagoCuentaEspecial)).RelatedSel(1)
 	// query k=v
 	for k, v := range query {
 		// rewrite dot-notation to Object__Attribute
